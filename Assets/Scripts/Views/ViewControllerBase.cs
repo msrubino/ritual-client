54c171a4e804e1b4956096024a07e6a2c7f6a9df
Jake OBrien <jake@flyclops.com>
2016-01-30 03:06:41 -0500

    Added delayed transition method

--- Assets/Scripts/Views/ViewControllerBase.cs
+++ Assets/Scripts/Views/ViewControllerBase.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public abstract class ViewControllerBase : MonoBehaviourBase
 {
 
+    protected void TransitionToView(float delay, ViewControllerBase view)
+    {
+        Delay(delay, () => 
+        {
+            TransitionToView(view);
+        });
+    }
+
     protected void TransitionToView(ViewControllerBase view)
     {
         GameObject.SetActive(false);
