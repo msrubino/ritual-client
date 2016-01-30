5cc7c7b824b403f6ebce83d98633cff5edc28f78
Jake OBrien <jake@flyclops.com>
2016-01-29 22:12:32 -0500

    Added abstract base ViewController class for transitions and common ViewController logic

---
+++ Assets/Scripts/Views/ViewControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ViewControllerBase : MonoBehaviourBase
+{
+
+    protected void TransitionToView(ViewControllerBase view)
+    {
+        GameObject.SetActive(false);
+        view.GameObject.SetActive(true);
+    }
+
+}
