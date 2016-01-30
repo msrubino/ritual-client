316423f4dc73918933274aec1486b8651753945b
Jake OBrien <jake@flyclops.com>
2016-01-30 03:15:54 -0500

    WonRound views

---
+++ Assets/Scripts/Views/AnnounceRoundWinnerViewController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnnounceRoundWinnerViewController : ViewControllerBase
+{
+
+    public void Start()
+    {
+        DelayedAdvanceToNextRound();
+    }
+
+    private void DelayedAdvanceToNextRound()
+    {
+        // if is leader
+        // TransitionToView(AppController.Instance.appTimes.roundEndAdvance,
+                            // AppController.Instance.viewReferences.leaderStartRoundView);
+        // else
+        TransitionToView(AppController.Instance.appTimes.roundEndAdvance,
+                         AppController.Instance.viewReferences.followerStartRoundView);
+    }
+
+}
