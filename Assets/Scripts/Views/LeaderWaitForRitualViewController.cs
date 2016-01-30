9994fc6dcf88ff560f25841200821f3f445704d9
Jake OBrien <jake@flyclops.com>
2016-01-30 09:29:12 -0500

    LeaderWaitForRitualViewController

---
+++ Assets/Scripts/Views/LeaderWaitForRitualViewController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderWaitForRitualViewController : ViewControllerBase
+{
+
+    public float timeToSync;
+
+    public void Start()
+    {
+        DelayedSync();
+    }
+
+    private void DelayedSync()
+    {
+        float delay = Mathf.Min(0f, timeToSync - Time.time);
+        Delay(delay, () => {
+            Sync();
+        });
+    }
+
+    private void Sync()
+    {
+        // sync
+        // ParseResponse();
+    }
+
+    private void ParseResponse()
+    {
+        // if reign won
+        // AdvanceToReignWon();
+        // else
+        // AdvanceToRoundWon();
+    }
+
+    private void AdvanceToReignWon()
+    {
+        TransitionToView(AppController.Instance.viewReferences.wonReignView);
+    }
+    
+    private void AdvanceToRoundWon()
+    {
+        TransitionToView(AppController.Instance.viewReferences.wonRoundView);
+    }
+
+}
