a27efca5ff4244acb57cddbb0b8da004bf68ed96
Jake OBrien <jake@flyclops.com>
2016-01-30 02:10:25 -0500

    RitualTypeMappings ScriptableObject to tie RitualType enum to prefabs to instantiate to implement ritual logic

---
+++ Assets/Scripts/App/RitualTypeMappings.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualTypeMappings : ScriptableObject 
+{
+
+    [System.Serializable]
+    public class Mapping
+    {
+        public RitualType ritualType;
+        public Ritual ritual;
+    }
+
+    public Mapping[] mappings;
+
+    public Ritual GetRitualForType(RitualType ritualType)
+    {
+        foreach (var mapping in mappings)
+        {
+            if (ritualType == mapping.ritualType)
+            {
+                return mapping.ritual;
+            }
+        }
+        return null;
+    }
+
+}
