4641849dc3fcae15e5b5414925d9c5c1bd46ef30
Jake OBrien <jake@flyclops.com>
2016-01-30 09:58:43 -0500

    Player model

---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿
+public class Player
+{
+
+    private string _uuid;
+    public string Uuid
+    {
+        get { return _uuid; }
+        set { _uuid = value; }
+    }
+
+    private string _username;
+    public string Username
+    {
+        get { return _username; }
+        set { _username = value; }
+    }
+
+}
