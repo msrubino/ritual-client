3d03e692d3a50fa4fd4f07699fd7f1c1609bdf3e
Jake OBrien <jake@flyclops.com>
2016-01-30 10:07:36 -0500

    Added polling times

--- Assets/Scripts/App/AppTimes.cs
+++ Assets/Scripts/App/AppTimes.cs
@@ -6,6 +6,8 @@ public class AppTimes
 {
 
     public float minSplashAdvance;
+    public float roundReadyPoll;
+    public float startRoundPoll;
     public float leaderStartReignAdvance;
     public float leaderStartRoundAdvance;
     public float roundEndAdvance;
