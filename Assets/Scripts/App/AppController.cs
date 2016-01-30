1c2847214e158f06d4252159594d136e98f0519d
Jake OBrien <jake@flyclops.com>
2016-01-29 22:10:07 -0500

    Added AppController stub class

---
+++ Assets/Scripts/App/AppController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppController : MonoBehaviour
+{
+
+    private static AppController _instance;
+    public static AppController Instance
+    {
+        get { return _instance; }
+    }
+
+    public void Awake()
+    {
+        _instance = this;
+    }
+
+}
