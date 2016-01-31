69436056f0fe097856218e86bfa5d7ae63c72842
Andrei Marks <andrei@weplaydots.com>
2016-01-31 10:58:50 -0500

    infohud debugging

---
+++ Assets/Scripts/App/InfoHud.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InfoHud : MonoBehaviourBase 
+{
+    public Text textField;
+
+    public void Log( string s )
+    {
+        textField.text = s;
+    }
+}
