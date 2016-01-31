e282bce1fb596d009d007951283c058da09d6c48
Michael Rubino <msrubino@gmail.com>
2016-01-30 23:16:36 -0500

    leader start round view

--- Assets/Scripts/Views/LeaderStartRoundViewController.cs
+++ Assets/Scripts/Views/LeaderStartRoundViewController.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class LeaderStartRoundViewController : ViewControllerBase
 {
+    [SerializeField]
+    Text _youAreChosenText;
 
     public void Start()
     {
+        _youAreChosenText.text = string.Format("{0}!", _players.playerPrefsNameKey);
         DelayedAdvanceToChooseRitual();
     }
 
     private void DelayedAdvanceToChooseRitual()
     {
-        TransitionToView(AppController.Instance.appTimes.leaderStartRoundAdvance,
-                         AppController.Instance.viewReferences.leaderChooseRitualView);
+        TransitionToView(_app.appTimes.leaderStartRoundAdvance,
+                         _app.viewReferences.leaderChooseRitualView);
     }
 
 }
