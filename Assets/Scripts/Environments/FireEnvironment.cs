dcf30821e6c180dae328a637fef7101001437bb3
Andrei Marks <andrei@weplaydots.com>
2016-02-12 09:23:17 -0500

    added reactive environment class, testing out fire environment

---
+++ Assets/Scripts/Environments/FireEnvironment.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireEnvironment : ReactiveEnvironment 
+{
+    public ParticleSystem volcanoSpout;
+
+    public override void OnInputFailed()
+    {
+        volcanoSpout.Pause();        
+    }
+
+    public override void OnInputSuccessful()
+    {
+        if ( !volcanoSpout.isPlaying ) volcanoSpout.Play();        
+    }
+}
---
+++ Assets/Scripts/Environments/ReactiveEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveEnvironment : MonoBehaviourBase 
+{
+    void OnEnable()
+    {
+        _eventSettings.OnInputFailed += OnInputFailed;    
+        _eventSettings.OnInputSuccessful += OnInputSuccessful;    
+    }
+
+    void OnDisable()
+    {
+        _eventSettings.OnInputFailed -= OnInputFailed;    
+        _eventSettings.OnInputSuccessful -= OnInputSuccessful;    
+    }
+
+    public virtual void OnInputFailed(){}
+    public virtual void OnInputSuccessful(){}
+}
