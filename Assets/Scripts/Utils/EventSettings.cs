90c59b067417d197caa1bfe9a7cede387ab8dfa9
Andrei Marks <andrei@weplaydots.com>
2016-02-12 09:22:10 -0500

    added event system for reactive environment signals.

---
+++ Assets/Scripts/Utils/EventSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class EventSettings : Settings<EventSettings> 
+{
+    public static void DefaultAction( Ritual ritual ){}
+
+    public event Action<Ritual> OnCurrentRitualSet = DefaultAction;
+    public void ReportCurrentRitualSet( Ritual ritual ) { OnCurrentRitualSet( ritual ); }
+
+    public event Action OnInputFailed = DefaultAction;
+    public void ReportInputFailed() { OnInputFailed(); }
+
+    public event Action OnInputSuccessful = DefaultAction;
+    public void ReportInputSuccessful() { OnInputSuccessful(); }
+
+    
+}
